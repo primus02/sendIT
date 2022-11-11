d10c622e39712b6ed9125617f9a0c1c382d3ea2e
Akinyemi Olalekan <69955888+primus02@users.noreply.github.com>
2022-11-11 17:58:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path= require("path");
 const cors= require("cors");
 
 const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 
 const customerRoute = require("./routes/signup");
